eb3330ef72992daad6387f2a3ec6ed2f67187191
forlo581230 <lores75634@gmail.com>
2017-10-23 03:09:15 +0800

    HW4

---
+++ atom.h
@@ -0,0 +1,33 @@
+#ifndef ATOM_H
+#define ATOM_H
+
+#include "term.h"
+
+class Atom : public Term{
+public:
+  Atom (string s):_symbol(s){}
+
+  string symbol() const{
+    return _symbol;
+  }
+  string value() const{return symbol();}
+
+  bool match(Term &term){
+    if(term.type()=="Variable" && term.value()==term.symbol()){
+      term.match(*this);
+    }
+    else if(term.value()!=term.symbol()){
+        return term.value()==value();
+    }
+    else{
+        return term.symbol()==_symbol;
+    }
+  }
+
+  string type() const{return "Atom";}
+private:
+  string _symbol;
+};
+
+
+#endif
---
+++ list.h
@@ -0,0 +1,79 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include "term.h"
+#include <vector>
+#include <iostream>
+using std::vector;
+
+class List : public Term {
+public:
+  string symbol() const{
+    if(_elements.empty())
+      return "[]";
+    else{
+      string ret ="[";
+      for(int i = 0; i < _elements.size() - 1 ; i++){
+        ret += _elements[i]-> symbol() + ", ";
+      }
+      ret += _elements[_elements.size()-1]-> symbol() + "]";
+      return  ret;
+    }
+  }
+  string value() const{
+    if(_elements.empty())
+      return "[]";
+    else{
+      string ret ="[";
+      for(int i = 0; i < _elements.size() - 1 ; i++){
+        ret += _elements[i]-> value() + ", ";
+      }
+      ret += _elements[_elements.size()-1]-> value() + "]";
+      return  ret;
+    }
+  }
+  bool match(Term & term){
+    List * ps = dynamic_cast<List *>(&term);
+
+    if(term.type()==type()){//list=list
+      if(ps->_elements.size()!= _elements.size())
+        return false;
+
+
+      for(int i = 0; i < _elements.size() - 1 ; i++){
+        if(!_elements[i]->match(*(ps->_elements[i])))
+          return false;
+      }
+
+      return true;
+    }
+  }
+
+  string type()const{
+    return "List";
+  }
+
+public:
+  List (): _elements(0){}
+  List (vector<Term *> const & elements):_elements(elements){}
+  Term * head() const{
+      //if(_elements.empty())return 0;
+      return *_elements.begin();
+  }
+  List * tail() const{
+    //if(_elements.empty())return 0;
+    //if(_elements.empty())throw "Accessing head in an empty list";
+    vector<Term *> args;
+    for(int i = 1; i < _elements.size() ; i++){
+      args.push_back(_elements[i]);
+    }
+    List *t=new List(args);
+    return t;
+  }
+
+private:
+  vector<Term *> _elements;
+
+};
+
+#endif
